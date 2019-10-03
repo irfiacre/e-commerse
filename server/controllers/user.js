5da9deea31cfacc6dbbcf87c1ce3bfc11e4fcff3
irfiacre <firaduka@outlook.com>
2019-10-04 00:13:23 +0200

    adding setups for the endpoints

---
+++ server/controllers/user.js
@@ -0,0 +1,12 @@
+import bcrypt from 'bcrypt';
+import '@babel/plugin-transform-regenerator';
+import '@babel/polyfill';
+import jwt from 'jsonwebtoken';
+
+class signing{
+    async signUp(req,res){
+        
+    }
+}
+
+export default new signing();
